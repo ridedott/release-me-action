7dae560bd341b071d345d6c996c3c57482308617
Natan Sągol <m@merlinnot.com>
2020-04-10 10:23:25 +0200

    fix: correctly release `ci` and `chore` types

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,8 @@ const parseOptions = {
  * https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular#type
  */
 const releaseRulesExtension = [
-  { release: 'patch', scope: 'deps', type: 'chore' },
+  { release: 'patch', type: 'ci' },
+  { release: 'patch', type: 'chore' },
   { release: 'patch', type: 'improvement' },
   { release: 'patch', type: 'refactor' },
 ];
