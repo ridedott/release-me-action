be0254415f3d25d52813e5668490a84f5b2c478b
Alex Cazacu <alexc@ridedott.com>
2020-06-11 15:03:22 +0200

    chore: output semantic version breakdown

--- src/utilities/outputParsers.spec.ts
+++ src/utilities/outputParsers.spec.ts
@@ -7,7 +7,7 @@ const setOutputSpy = jest.spyOn(actionsCore, 'setOutput').mockImplementation();
 
 describe('reportResults', (): void => {
   it('sets output based on nextRelease', (): void => {
-    expect.assertions(5);
+    expect.assertions(8);
 
     const input: Result = {
       commits: [],
@@ -29,13 +29,44 @@ describe('reportResults', (): void => {
     const result = reportResults(input);
 
     expect(result).toBeUndefined();
-    expect(setOutputSpy).toHaveBeenCalledTimes(3);
+    expect(setOutputSpy).toHaveBeenCalledTimes(6);
     expect(setOutputSpy).toHaveBeenCalledWith(
       'version',
       input.nextRelease.version,
     );
     expect(setOutputSpy).toHaveBeenCalledWith('level', input.nextRelease.type);
     expect(setOutputSpy).toHaveBeenCalledWith('released', 'true');
+
+    expect(setOutputSpy).toHaveBeenCalledWith('major', '1');
+    expect(setOutputSpy).toHaveBeenCalledWith('minor', '1');
+    expect(setOutputSpy).toHaveBeenCalledWith('patch', '1');
+  });
+
+  it('sets prerelease and meta outputs if they are included in the version', (): void => {
+    expect.assertions(3);
+
+    const input: Result = {
+      commits: [],
+      lastRelease: {
+        gitHead: 'refs/heads/master',
+        gitTag: '1.1.0',
+        version: '1.1.0',
+      },
+      nextRelease: {
+        gitHead: 'refs/heads/master',
+        gitTag: '1.1.1-prerelease+build',
+        notes: 'Note',
+        type: 'patch',
+        version: '1.1.1-prerelease+build',
+      },
+      releases: [],
+    };
+
+    reportResults(input);
+
+    expect(setOutputSpy).toHaveBeenCalledTimes(8);
+    expect(setOutputSpy).toHaveBeenCalledWith('pre-release', 'prerelease');
+    expect(setOutputSpy).toHaveBeenCalledWith('build', 'build');
   });
 
   it('throws an error if there is no released version', (): void => {
