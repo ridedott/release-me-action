8f7f53a1dd3412327e8aa7cfefc1528848133a0e
Andrés C. Viesca Ruiz <andres.viescar@gmail.com>
2020-08-11 13:33:26 +0200

    test: add signale mock

---
+++ __mocks__/signale.ts
@@ -0,0 +1,15 @@
+/* eslint-disable class-methods-use-this */
+
+import * as signale from 'signale';
+
+class Signale extends signale.Signale {
+  /* eslint-disable @typescript-eslint/naming-convention */
+  public _log(): void {}
+
+  public _logger(): void {}
+
+  public _write(): void {}
+  /* eslint-enable @typescript-eslint/naming-convention */
+}
+
+export { Signale };
