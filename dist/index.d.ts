01b254ec69729498fab20f4492d5186c37449f26
Joachim Roeleveld <joachimroeleveld@gmail.com>
2020-12-18 11:50:41 +0100

    chore: add build files

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import { Config, Options, Result } from 'semantic-release';
+export declare const release: (overrideOptions?: Options | undefined, overrideConfig?: Config | undefined) => Promise<Result>;
---
+++ dist/utilities/inputProcessors.d.ts
@@ -0,0 +1,33 @@
+import { BranchSpec } from 'semantic-release';
+export declare enum InputParameters {
+    CommitAssets = "commit-assets",
+    ConfigFile = "config-file",
+    DisableChangelog = "disable-generate-changelog",
+    DryRun = "dry-run",
+    NodeModule = "node-module",
+    ReleaseAssets = "release-assets",
+    ReleaseBranches = "release-branches",
+    ReleaseRules = "release-rules"
+}
+export interface ReleaseRule {
+    release: string | false;
+    scope?: string;
+    subject?: string;
+    type?: string;
+}
+/**
+ * These rules extend the default rules provided by commit-analyzer.
+ * Added rules are types supported by commitizen but not supported in standard
+ * commit-analyzer. Rules are based on Angular contribution guidelines:
+ * https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular#type
+ */
+export declare const DEFAULT_RELEASE_RULES: ReleaseRule[];
+export declare const processInputNodeModule: () => boolean;
+export declare const processInputDisableChangelog: () => boolean;
+export declare const processInputDryRun: () => boolean;
+export declare const processInputReleaseBranches: () => BranchSpec[] | undefined;
+export declare const processInputConfigFile: () => string | undefined;
+export declare const processInputReleaseRules: () => ReleaseRule[];
+export declare const processInputCommitAssets: () => string[];
+export declare const processInputReleaseAssets: () => string[];
+//# sourceMappingURL=inputProcessors.d.ts.map
