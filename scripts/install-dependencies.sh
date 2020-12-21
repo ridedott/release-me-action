a6aacc0121af76ca0de6d9d90d64bb6add0748af
Joachim Roeleveld <joachimroeleveld@gmail.com>
2020-12-21 11:09:47 +0100

    fix: dep script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,13 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 npm --prefix "$1" ci "$1" --only=production --no-audit --no-progress --prefer-offline
 
-packages=""
-for (( i = 1; i < "$#"; i++ )); do
-    packages+=" $i"
-done
-
 if [ "${#$}" -gt 1 ]; then
   # Install additional packages
-  npm --prefix "$1" i "$1" "$packages" --no-audit --no-progress --prefer-offline
+  npm --prefix "$1" i "$1" "${@:2}" --no-audit --no-progress --prefer-offline
 fi
