a05f66e49cd852654665367b51331854bee0107e
Alex Cazacu <alexc@ridedott.com>
2021-01-07 10:42:31 +0100

    chore: update dependabot login

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
 npm --prefix "$1" ci "$1" --only=production --no-audit --no-progress --prefer-offline
-
-if [ "${#$}" -gt 1 ]; then
-  # Install additional packages
-  npm --prefix "$1" i "$1" "${@:2}" --no-audit --no-progress --prefer-offline
-fi
