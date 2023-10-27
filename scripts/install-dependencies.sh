8dd9be0e04ccb3dd96cdd8db145591539d1d948b
maartenRamadan <maartenramadan@gmail.com>
2023-10-27 14:00:11 +0200

    fix: restore install dependency script

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+npm --prefix "$1" ci "$1" --only=production --no-audit --no-progress --prefer-offline
+
 if [ "$#" -gt 1 ]; then
   # Install additional packages.
   npm --prefix "$1" install "$1" "${@:2}" --no-audit --no-progress --prefer-offline
