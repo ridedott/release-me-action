e97c4fe11fb3da58b4ce707b1ca5b03f572963d0
Julien Ganichot <julien.ganichot@gmail.com>
2023-10-31 16:27:18 +0100

    fix: deprecated dep

--- src/utilities/inputProcessors.ts
+++ src/utilities/inputProcessors.ts
@@ -1,5 +1,5 @@
 import { getInput } from '@actions/core';
-import joi from '@hapi/joi';
+import joi from 'joi';
 import { BranchSpec } from 'semantic-release';
 
 const MATCH_CONFIG_FILE_EXTENSION_REGEXP = /\.(?:ya?ml|js)$/u;
