a75df47e10f119d2ccd18e82bef2fa54910bea15
Oleksandr Mykolaichuk <oleksandr@ridedott.com>
2023-11-21 11:16:01 +0100

    debug: package root path

--- src/utilities/generatePlugins.ts
+++ src/utilities/generatePlugins.ts
@@ -18,71 +18,73 @@ export const generatePlugins = ({
   packageRoot: string;
   releaseAssets: string[];
   releaseRules: ReleaseRule[];
-}): PluginSpec[] /* eslint-disable unicorn/prevent-abbreviations */ => [
-  [
-    '@semantic-release/commit-analyzer',
-    {
-      parserOpts: {
-        mergeCorrespondence: ['id', 'source'],
-        mergePattern: /^Merge pull request #\d+ from .*$/u,
+}): PluginSpec[] /* eslint-disable unicorn/prevent-abbreviations */ => {
+  console.log("root: ", packageRoot);
+  return [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        parserOpts: {
+          mergeCorrespondence: ['id', 'source'],
+          mergePattern: /^Merge pull request #\d+ from .*$/u,
+        },
+        preset: 'angular',
+        releaseRules,
       },
-      preset: 'angular',
-      releaseRules,
-    },
-  ],
-  [
-    '@semantic-release/release-notes-generator',
-    {
-      preset: 'angular',
-      writerOpts: {
-        transform,
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        writerOpts: {
+          transform,
+        },
       },
-    },
-  ],
-  ...(disableChangeLog === false
-    ? [
-        '@semantic-release/changelog',
-        [
-          '@semantic-release/exec',
-          {
-            prepareCmd: 'npx prettier --parser markdown --write CHANGELOG.md',
-          },
-        ] as PluginSpec,
-      ]
-    : []),
-  ...(isNodeModule === true
-    ? [
-        [
-          '@semantic-release/npm',
-          {
-            npmPublish: false,
-            pkgRoot: packageRoot,
-          },
-        ] as PluginSpec,
-      ]
-    : []),
-  [
-    '@semantic-release/git',
-    {
-      assets: [
-        ...(disableChangeLog === false ? ['./CHANGELOG.md'] : []),
-        ...commitAssets,
-        ...(isNodeModule
-          ? ['./package.json', './package-lock.json', './yarn-lock.yaml']
-          : []),
-      ],
-      /* eslint-disable-next-line no-template-curly-in-string */
-      message: 'chore(release): v${nextRelease.version}',
-    },
-  ],
-  [
-    '@semantic-release/github',
-    {
-      assets: releaseAssets,
-      failComment: false,
-      releasedLabels: false,
-      successComment: false,
-    },
-  ],
-];
+    ],
+    ...(disableChangeLog === false
+      ? [
+          '@semantic-release/changelog',
+          [
+            '@semantic-release/exec',
+            {
+              prepareCmd: 'npx prettier --parser markdown --write CHANGELOG.md',
+            },
+          ] as PluginSpec,
+        ]
+      : []),
+    ...(isNodeModule === true
+      ? [
+          [
+            '@semantic-release/npm',
+            {
+              npmPublish: false,
+              pkgRoot: packageRoot,
+            },
+          ] as PluginSpec,
+        ]
+      : []),
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          ...(disableChangeLog === false ? ['./CHANGELOG.md'] : []),
+          ...commitAssets,
+          ...(isNodeModule
+            ? ['./package.json', './package-lock.json', './yarn-lock.yaml']
+            : []),
+        ],
+        /* eslint-disable-next-line no-template-curly-in-string */
+        message: 'chore(release): v${nextRelease.version}',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: releaseAssets,
+        failComment: false,
+        releasedLabels: false,
+        successComment: false,
+      },
+    ],
+]};
 /* eslint-enable unicorn/prevent-abbreviations */
